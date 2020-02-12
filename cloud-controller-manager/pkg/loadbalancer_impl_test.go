085d6ef477d2168902af7e33ef4ffd378e7b096d
Cria Hu <huyuqing@inspur.com>
2020-02-12 14:48:41 +0800

    unit test

--- cloud-controller-manager/pkg/loadbalancer_impl_test.go
+++ cloud-controller-manager/pkg/loadbalancer_impl_test.go
@@ -6,46 +6,9 @@ import (
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"testing"
-	"time"
 )
-var (
-	vpcId                     = "vpc-2zeaybwqmvn6qgabfd3pe"
-	regionId                  = "cn-beijing"
-	zoneId                    = "cn-beijing-b"
-	certID                    = "1745547945134207_157f665c830"
-	listenPort1         int32 = 80
-	listenPort2         int32 = 90
-	targetPort1               = intstr.FromInt(8080)
-	targetPort2               = intstr.FromInt(9090)
-	nodePort1           int32 = 8080
-	nodePort2           int32 = 9090
-	protocolTcp               = v1.ProtocolTCP
-	protocolUdp               = v1.ProtocolUDP
-	node1                     = "i-bp1bcl00jhxr754tw8vx"
-	node2                     = "i-bp1bcl00jhxr754tw8vy"
-	clusterName               = "clusterName-random"
-	serviceUIDNoneExist       = "UID-1234567890-0987654321-1234556"
-	serviceUIDExist           = "c83f8bed-812e-11e9-a0ad-00163e0a3984"
-	nodeName                  = "iZuf694l8lw6xvdx6gh7tkZ"
 
-)
-type Context2 struct{
-   key string
-}
-func (c *Context2)Va(r context.Context)interface{}{
-	return r.Value(c.key)
-}
-func (c *Context2)Er(r context.Context)error{
-	return r.Err()
-}
-func (c *Context2)Do(r context.Context)<-chan struct{}{
-	return r.Done()
-}
-func (c *Context2)DL(r context.Context)(deadline time.Time, ok bool){
-	return r.Deadline()
-}
 func TestEnsureLoadBalancerDeleted1(t *testing.T) {
 	clusterName := ""
 	service := &v1.Service{}
@@ -62,8 +25,24 @@ func TestEnsureLoadBalancerDeleted1(t *testing.T) {
 			{SLBId: "1234"},
 		}, nil
 	})
+	patch3 := ApplyFunc(GetListenerForPort,func (existingListeners []Listener, port v1.ServicePort) *Listener {
+		return &Listener{
+			SLBId:"1",
+		}
+	})
+	patch4:=ApplyFunc(GetBackends,func (config *InCloud, slbid, listenerId string) ([]Backend, error) {
+		return []Backend{
+			{BackendId:"11"},
+		},nil
+	})
+	patch5:=ApplyFunc(DeleteBackends,func (config *InCloud, slbid, listenerId string, backendIdList []string) error {
+		return nil
+	})
 	defer patch1.Reset()
 	defer patch2.Reset()
+	defer patch3.Reset()
+	defer patch4.Reset()
+	defer patch5.Reset()
 	c.EnsureLoadBalancerDeleted(context.TODO(), clusterName, service)
 }
 
--- cloud-controller-manager/pkg/loadbalancer_test.go
+++ cloud-controller-manager/pkg/loadbalancer_test.go
@@ -1,11 +1,19 @@
 package pkg
 
 import (
-	."github.com/agiledragon/gomonkey"
+	. "github.com/agiledragon/gomonkey"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"testing"
 )
 
+var (
+	listenPort1         int32 = 80
+	targetPort1               = intstr.FromInt(8080)
+	nodePort1           int32 = 8080
+	clusterName               = "clusterName-random"
+	serviceUIDNoneExist       = "UID-1234567890-0987654321-1234556"
+)
 func TestGetLoadBalancer(t *testing.T) {
 	config :=&InCloud{}
 	service :=&v1.Service{}
@@ -24,5 +32,29 @@ func TestGetLoadBalancer(t *testing.T) {
 	defer patch1.Reset()
 	defer patch2.Reset()
 	defer patch3.Reset()
-	GetLoadBalancer(config,service)
+	lb,err:=GetLoadBalancer(config,service)
+	if lb==nil||err!=nil{
+		t.Fatal("get load balancer failed")
+	}
+}
+
+func TestDeleteLoadBalancer(t *testing.T) {
+	config :=&InCloud{}
+	service := &v1.Service{}
+	patch1:=ApplyFunc(getServiceAnnotation,func (service *v1.Service, annotationKey string, defaultSetting string) string {
+		return "12"
+	})
+	patch2:=ApplyFunc(getKeyCloakToken,func (requestedSubject, tokenClientId, clientSecret, keycloakUrl string, ic *InCloud) (string, error) {
+		return "",nil
+	})
+	patch3:=ApplyFunc(deleteLoadBalancer,func (url, token, slbId string) error {
+		return nil
+	})
+	defer patch1.Reset()
+	defer patch2.Reset()
+	defer patch3.Reset()
+	err := DeleteLoadBalancer(config,service)
+	if err !=nil{
+		t.Fatal(err)
+	}
 }
